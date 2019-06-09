84739a89ccf39c59656598e8aebe42a7eca8b1ca
zionward <ziyingwang2019@u.northwestern.edu>
2019-06-09 16:48:05 -0500

    change config.py for RDS

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ DATABASE_NAME = 'msia423'
 SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.format(conn_type=conn_type, user=user, password=password, host=host, port=port, DATABASE_NAME=DATABASE_NAME)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-HOST = "127.0.0.1"
+# HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 MODEL_PATH = "models/logreg.pkl"
 DATA_PATH = "data/admission_to_train.csv"
